a1f7e1aa734a90248cababaaf97b09920989444e
bdurb <brian@rpmgreenville.com>
2018-11-28 15:51:11 -0800

    set up axios call to cat api, displaying random cat pictures to site

--- src/components/Cats.vue
+++ src/components/Cats.vue
@@ -1,10 +1,29 @@
 <template>
+  <div>
+    <h1>Lets Get some Cats</h1>
+    <button v-on:click="fetchCats">New Cat</button><br/>
+    <img :src="cats" />
+  </div>
   
 </template>
 
 <script>
 export default {
-  name: "Cats"
+  name: "Cats",
+  data () {
+    return {
+      cats: ''
+    }
+  },
+  methods: {
+    fetchCats() {
+      const url = 'https://aws.random.cat/meow'
+      this.$http.get(url)
+      .then((res) => {
+        this.cats = res.data.file
+      })
+    }
+  }
 }
 </script>
 
