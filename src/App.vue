0d074a4f2b8fb0650d0c2788d4281d4d9d7f4eda
bdurb <brian@rpmgreenville.com>
2018-11-28 15:24:28 -0800

    removed boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cats />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cats from './components/Cats.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Cats
   }
 }
 </script>
